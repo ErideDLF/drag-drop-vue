cb765cfda84733d3e6b14e799f1a51757e0ca2a6
Irene <delafuente.irene@gmail.com>
2017-06-01 17:43:16 +0200

    [electron] primer commit

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <main-container></main-container>
+  </div>
+</template>
+
+<script>
+  import MainContainer from 'components/MainContainer/MainContainer';
+  export default {
+    components: {
+      MainContainer,
+    },
+  };
+</script>
+
+<style lang="scss">
+  @import '~bulma';
+  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body { height: 100%; }
+
+  body {
+    align-items: center;
+    background:
+      radial-gradient(
+        ellipse at center,
+        rgba(255, 255, 255, 1) 0%,
+        rgba(229, 229, 229, .85) 100%
+      );
+    background-position: center;
+    display: flex;
+    font-family: Lato, Helvetica, sans-serif;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
---
+++ app/src/renderer/components/MainContainer/MainContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Primary title
+          </h1>
+          <h2 class="subtitle">
+            Primary subtitle
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="columns box">
+        <div class="column">
+          <div class="field">
+            <label class="label">Name</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="nombre del zip">
+            </p>
+          </div>
+          <div class="field is-grouped">
+            <p class="control">
+              <button class="button is-primary">Submit</button>
+            </p>
+            <p class="control">
+              <button class="button is-link">Cancel</button>
+            </p>
+          </div>
+        </div>
+        <div class="column is-three-quarters">
+          Second column
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-container',
+  };
+</script>
+
+<style scoped>
+</style>
